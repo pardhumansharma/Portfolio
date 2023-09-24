1d1e140a928cf2198e615ba3d0a617a6abcaf581
Pardhuman Sharma <113529594+pardhumansharma@users.noreply.github.com>
2023-09-24 22:23:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -48,28 +48,6 @@ $(document).ready(function(){
         loop: true
     });
 var slideIndex = 0;
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    
-    slideIndex++;
-    
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    
-    slides[slideIndex - 1].style.display = "none";
-    
-    setTimeout(showSlides, 1500); // Change slide every 2 seconds (2000 milliseconds)
-}
-
-showSlides();
-
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
